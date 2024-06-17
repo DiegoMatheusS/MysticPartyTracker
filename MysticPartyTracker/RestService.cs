15cc5e5eaa9f940a6a73d0cccc4932bba12b6944
Diego Matheus <dms.diego.2019@gmail.com>
2024-06-17 20:35:38 -0300

    adicionado metodo buscar spells

---
+++ MysticPartyTracker/RestService.cs
@@ -0,0 +1,52 @@
+﻿using MysticPartyTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MysticPartyTracker
+{
+    class RestService
+    {
+        HttpClient _client;
+        JsonSerializerOptions _serializerOptions;
+
+        public ObservableCollection<Result> Count { get; set; }
+
+        public RestService()
+        {
+            Count = null;
+            _client = new HttpClient();//instaciei  _client
+            _serializerOptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                WriteIndented = true,
+
+            };
+         }
+        public async Task<ObservableCollection<Result>> GetSpellsAsync()//Tarefa = 'Task'// É uma lista que devolve 
+        {
+            Uri uri = new Uri("https://www.dnd5eapi.co/api/spells");// onde vou buscar informação "endereço" 'URL'
+
+            try
+            {
+                HttpResponseMessage response = await _client.GetAsync(uri);
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync();
+                    Count = JsonSerializer.Deserialize<ObservableCollection<Result>>(content, _serializerOptions);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+            return Count ?? [];
+        }
+    }
+ }
+
---
+++ MysticPartyTracker/ViewModels/SpellsViewModel.cs
@@ -0,0 +1,45 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using MysticPartyTracker.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+
+namespace MysticPartyTracker.ViewModels
+{
+    public partial class SpellsViewModel : ObservableObject
+    {
+
+        private readonly RestService _restService;
+
+        [ObservableProperty]
+        private ObservableCollection<Result> _results = new ObservableCollection<Result>();
+
+
+        public SpellsViewModel()
+        {
+            _restService = new RestService();
+            CreateSpellsCommand = new Command(async () => await GetSpellsAsync());
+ 
+        }
+
+        
+           
+        public ICommand CreateSpellsCommand { get; } // pegar comando do botao
+
+
+        public async Task GetSpellsAsync()
+        {
+            Results = await _restService.GetSpellsAsync();
+        }
+
+    }
+}
